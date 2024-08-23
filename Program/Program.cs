7fe770aa7915e2bd20e585b4bd5386594b531920
ammaster10s <pawaris_avira@hotmail.com>
2024-08-23 13:04:37 +0700

    LAB01

---
+++ Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void GreetUser(string name)
+    {
+        Console.WriteLine($"Hello, {name}! Welcome");
+
+    }
+    public static void Main()
+    {
+        string userName = "John";
+        GreetUser(userName);
+    }
+}
